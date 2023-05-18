d976920f8e5b2f77ede80e99d619777081d27752
ahmed fathy <ahmed.fathy9707@gmail.com>
2023-05-18 11:13:15 +0300

    first commit

---
+++ lib/presentation/screens/home_screens/nearest_stations.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NearestStation extends StatelessWidget {
+  const NearestStation({super.key});
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+     
+  }
+}
